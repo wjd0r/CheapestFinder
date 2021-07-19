c97753b59b1297888c5f77c054e3834baa9023e1
jyk86 <jyk86@cyber-i.com>
2021-07-20 08:50:46 +0900

    ``

--- js/tensorflow.js
+++ js/tensorflow.js
@@ -13,15 +13,17 @@ class MyTensorflow {
     ys = [];
     mingap = 99999999999;
     compute = 0;
+    keys = {};
 
     // 학습할 데이터를 각각의 배열에 담기
-    init = (stocks, completed, ing) => {
+    init = (stocks, completed, ing, keys) => {
+        this.keys = keys;
         this.stocks = stocks;
 
-        this.x1s = this.stocks.map(value => parseInt(value.MMEND_CLSPRC.replace(/,/g , '')));
-        this.x2s = this.stocks.map(value => parseInt(value.HGST_CLSPRC.replace(/,/g , '')));
-        this.x3s = this.stocks.map(value => parseInt(value.LWST_CLSPRC.replace(/,/g , '')));
-        this.ys = this.stocks.map(value => parseInt(value.HGST_CLSPRC.replace(/,/g , '')));
+        this.x1s = this.stocks.map(value => parseInt(value[this.keys.x1].replace(/,/g , '')));
+        this.x2s = this.stocks.map(value => parseInt(value[this.keys.x2].replace(/,/g , '')));
+        this.x3s = this.stocks.map(value => parseInt(value[this.keys.x3].replace(/,/g , '')));
+        this.ys = this.stocks.map(value => parseInt(value[this.keys.y].replace(/,/g , '')));
 
         this.x1s.shift(); // 처음  요소 삭제
         this.x2s.shift(); // 처음  요소 삭제
@@ -36,11 +38,11 @@ class MyTensorflow {
         const xs2 = tf.tensor(x2);
         const xs3 = tf.tensor(x3);
 
-        const mx1 = xs1.mul(this.state.m1);
-        const mx2 = xs2.mul(this.state.m2);
-        const mx3 = xs3.mul(this.state.m3);
+        const mx1 = xs1.mul(this.m1);
+        const mx2 = xs2.mul(this.m2);
+        const mx3 = xs3.mul(this.m3);
 
-        const ys = mx1.add(mx2).add(mx3).add(this.state.b);
+        const ys = mx1.add(mx2).add(mx3).add(this.b);
 
         return ys;
     }
@@ -50,7 +52,7 @@ class MyTensorflow {
     start = (completed, ing) => {
         tf.tidy(() => {
             for(let i = 0; i < 10; i++) {
-                optimizer.minimize(() => loss(predict(x1s, x2s, x3s), tf.tensor(ys)));
+                this.optimizer.minimize(() => this.loss(this.predict(this.x1s, this.x2s, this.x3s), tf.tensor(this.ys)));
             }
         });
         this.restart(completed, ing);
@@ -58,20 +60,20 @@ class MyTensorflow {
 
     restart(completed, ing) {
         // 학습데이터의 예측값
-        let predict_y = Number(String(predict(parseFloat(x1s[0]),parseFloat(x2s[0]),parseFloat(x3s[0]))).replace('Tensor','').replace(/a/gi,""));
+        let predict_y = Number(String(this.predict(parseFloat(this.x1s[0]),parseFloat(this.x2s[0]),parseFloat(this.x3s[0]))).replace('Tensor','').replace(/a/gi,""));
         // 예측값 - 실제 값
-        let gap = Math.abs(predict_y - ys[0]);
+        let gap = Math.abs(predict_y - this.ys[0]);
         // 예측해야 될 데이터
-        let x1 = parseFloat(stocks[1].MMEND_CLSPRC.replace(/,/g, ''));
-        let x2 = parseFloat(stocks[1].HGST_CLSPRC.replace(/,/g, ''));
-        let x3 = parseFloat(stocks[1].LWST_CLSPRC.replace(/,/g, ''));
+        let x1 = parseFloat(this.stocks[1][this.keys.x1].replace(/,/g, ''));
+        let x2 = parseFloat(this.stocks[1][this.keys.x2].replace(/,/g, ''));
+        let x3 = parseFloat(this.stocks[1][this.keys.x3].replace(/,/g, ''));
         // 아직 최소 갭이 아니라면
-        let if1 = mingap > gap;
+        let if1 = this.mingap > gap;
         // 너무 빨리 끝났다면
-        let if2 = compute < 5;
+        let if2 = this.compute < 5;
 
         if(if1) {
-            let gap2 = Math.abs(mingap - gap);
+            let gap2 = Math.abs(this.mingap - gap);
             let t = 1;
                  if(gap > 1000) { t = 1.4; }
             else if(gap > 100) { t = 1.3; }
@@ -79,20 +81,20 @@ class MyTensorflow {
             else if(gap > 1) { t = 1.1; }
             else if(gap > 0.1) { t = 1; }
 
-            this.mingap = mingap < gap ? mingap : gap;
-            this.learnignRate = learnignRate * t;
-            this.optimizer = tf.train.sgd(learnignRate);
-            this.compute = compute+1;
+            this.mingap = this.mingap < gap ? this.mingap : gap;
+            this.learnignRate = this.learnignRate * t;
+            this.optimizer = tf.train.sgd(this.learnignRate);
+            this.compute = this.compute+1;
 
-            ing(y);
+            ing(this.y);
 
-            this.y = Number(String(predict(x1,x2,x3)).replace('Tensor','').replace(/a/gi,""));
+            this.y = Number(String(this.predict(x1,x2,x3)).replace('Tensor','').replace(/a/gi,""));
 
-            setTimeout(() => start(), 1);
+            setTimeout(() => this.start(completed, ing), 1);
         } else if (if2) {
 
-            this.learnignRate = learnignRate * 10;
-            this.optimizer = tf.train.sgd(learnignRate * 10);
+            this.learnignRate = this.learnignRate * 10;
+            this.optimizer = tf.train.sgd(this.learnignRate * 10);
             this.m1 = tf.variable(tf.scalar(Math.random(1)));
             this.m2 = tf.variable(tf.scalar(Math.random(1)));
             this.m3 = tf.variable(tf.scalar(Math.random(1)));
@@ -100,9 +102,9 @@ class MyTensorflow {
             this.mingap = 99999999999;
             this.compute = 0;
 
-            setTimeout(() => start(), 1);
+            setTimeout(() => this.start(completed, ing), 1);
         } else {
-            completed(m1, m2, m3, b);
+            completed(this.m1, this.m2, this.m3, this.b);
         }
     }
 }
